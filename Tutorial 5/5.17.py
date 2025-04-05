469003e8d3ae197599e4c6bd5d4a69fbd52457c2
DevikaP22 <devik36777015@gmail.com>
2025-04-05 16:04:00 +0530

    Update 5.17.py

--- Tutorial 5/5.17.py
+++ Tutorial 5/5.17.py
@@ -1,27 +1,13 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-df = pd.read_csv("weather.csv")
-print("First 10 rows of weather data:")
-print(df.head(10))
-max_temperature = df["temperature"].max()
-min_temperature = df["temperature"].min()
-print("\nMaximum temperature:", max_temperature)
-print("Minimum temperature:", min_temperature)
-places_less_than_28 = df[df["temperature"] < 28][["place", "temperature"]]
-print("\nPlaces with temperature less than 28°C:")
-print(places_less_than_28)
-cloudy_places = df[df["weather"] == "Cloudy"][["place", "weather"]]
-print("\nPlaces with weather = 'Cloudy':")
-print(cloudy_places)
-weather_frequency = df["weather"].value_counts().sort_index()
-print("\nWeather frequency:")
-print(weather_frequency)
-plt.figure(figsize=(10,6))
-plt.bar(df["date"], df["temperature"])
-plt.xlabel("Date")
-plt.ylabel("Temperature")
-plt.title("Temperature of each day")
-plt.xticks(rotation=90)
-plt.tight_layout()
-plt.show()
+mport pandas as pd
+df = pd.read_csv("student.csv")
+average_cgpa = df["CGPA"].mean()
+print(" Average CGPA of Students:", round(average_cgpa, 2))
+high_cgpa_students = df[df["CGPA"] > 9]
+print("\n Students with CGPA > 9:\n", high_cgpa_students)
+cse_high_cgpa = df[(df["Branch"] == "CSE") & (df["CGPA"] > 9)]
+print("\n CSE Students with CGPA > 9:\n", cse_high_cgpa)
+max_cgpa_student = df[df["CGPA"] == df["CGPA"].max()]
+print("\n Student with Maximum CGPA:\n", max_cgpa_student)
+branch_avg_cgpa = df.groupby("Branch")["CGPA"].mean()
+print("\n Average CGPA of Each Branch:\n", branch_avg_cgpa)
 
